db48fa987011cf9d4078694740737d5b3ed49e85
Alex <147708650+xuzz0407@users.noreply.github.com>
2026-07-10 15:47:10 +0800

    Time: 312 ms (36.29%), Space: 50.2 MB (45.16%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
@@ -0,0 +1,32 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        rx, ry = self.find(x), self.find(y)
+        if rx == ry:
+            return
+        if self.rank[rx] < self.rank[ry]:
+            rx, ry = ry, rx
+        self.parent[ry] = rx
+        if self.rank[rx] == self.rank[ry]:
+            self.rank[rx] += 1
+
+
+class Solution:
+    def pathExistenceQueries(
+        self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]
+    ) -> List[bool]:
+        dsu = DSU(n)
+
+        for i in range(1, n):
+            if nums[i] - nums[i-1] <= maxDiff:
+                dsu.union(i-1, i)
+
+        return [dsu.find(u) == dsu.find(v) for u, v in queries]
