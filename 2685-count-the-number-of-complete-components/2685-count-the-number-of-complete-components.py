8c143a33cdb009cd6c11174201821df24763905b
Alex <147708650+xuzz0407@users.noreply.github.com>
2026-07-11 20:09:43 +0800

    Time: 27 ms (90.99%), Space: 20.6 MB (44.57%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        g = [[] for _ in range(n)]
+        for x, y in edges:
+            g[x].append(y)
+            g[y].append(x)
+
+        vis = [False] * n
+
+        def dfs(x: int) -> None:
+            nonlocal v, e
+            v += 1
+            e += len(g[x])
+
+            vis[x] = True
+            for y in g[x]:
+                if not vis[y]:
+                    dfs(y)
+
+        ans = 0
+        for i, b in enumerate(vis):
+            if not b:
+                v = e = 0
+                dfs(i)
+                ans += e == v * (v - 1)
+        return ans
+
