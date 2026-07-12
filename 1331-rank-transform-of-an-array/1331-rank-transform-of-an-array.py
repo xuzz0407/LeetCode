6c15ff1151cdb4bc400280d7e6c261ceaffe6784
Alex <147708650+xuzz0407@users.noreply.github.com>
2026-07-12 21:19:01 +0800

    Time: 34 ms (79.48%), Space: 37.7 MB (21.83%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,4 +1,3 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        rk = {x: i for i, x in enumerate(sorted(set(arr)), 1)}
-        return [rk[x] for x in arr]
+        return list(map({x: i for i, x in enumerate(sorted(set(arr)), 1)}.__getitem__, arr))
